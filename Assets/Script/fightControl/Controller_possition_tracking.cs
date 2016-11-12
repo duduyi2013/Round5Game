ad5b156bce7892949cf13d13e10e6efec869f9e2
Luo Heng <luoheng0806@163.com>
2016-11-12 17:49:10 -0500

    add script by lucas into the game

---
+++ Assets/Script/fightControl/Controller_possition_tracking.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller_possition_tracking : MonoBehaviour {
+    public GameObject right_Controller;
+    public Vector3 velocity;
+    Vector3 _lastFramePosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = right_Controller.transform.position;
+	}
+    void FixedUpdate()
+    {
+        velocity = (this.transform.position - _lastFramePosition) / Time.fixedDeltaTime;
+        _lastFramePosition = this.transform.position;
+    }
+}
