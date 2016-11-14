cefe41362599462c70633e8721ab33d532c0e6df
MSI\JIn <hanjinchao416@gmail.com>
2016-11-15 05:08:58 +0800

    add some sound files

---
+++ Assets/Script/characterContrl/PlayCharacterSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCharacterSound : MonoBehaviour {
+	AudioSource _myAud;
+	public AudioClip _rollingClip;
+
+	void Start(){
+		_myAud = GetComponent<AudioSource> ();
+	}
+
+	void PlayRolling(float _volume = 1.0f){
+		_myAud.clip = _rollingClip;
+		_myAud.volume = _volume;
+		_myAud.Play();
+	}	
+}
---
+++ Assets/Script/characterContrl/PlayEyeSounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayEyeSounds : MonoBehaviour {
+	AudioSource _myAud;
+	public AudioClip _eyeRoar;
+	float _latency = 0.5f;
+	public int _myIndex;
+
+	void Start(){
+		_myAud = GetComponent<AudioSource> ();
+		if (name != "endScene") {
+			PlayRolling (_myIndex * _latency);
+		}
+	}
+
+	void PlayRolling(float _ltn){
+		_myAud.clip = _eyeRoar;
+		_myAud.volume = 1f;
+		_myAud.PlayDelayed(_ltn);
+	}	
+}
